e7b2e4b2a278cc6837fc88dc76da9a115a742c45
Vortes <zeyu.weng@gmail.com>
2019-11-25 13:42:24 -0500

    Initial Commit

---
+++ Workout Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Workout Tracker
+//
+//  Created by Alan Weng on 11/25/19.
+//  Copyright © 2019 Alan Weng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
