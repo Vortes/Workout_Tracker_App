4bf6a9e3d6e851885b3b97020fdb09ea07597ec2
Vortes <zeyu.weng@gmail.com>
2019-11-27 13:21:06 -0500

    Aesthetic Changes

---
+++ Workout Tracker/HostingController.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+import SwiftUI
+
+class HostingController: UIHostingController<ContentView> {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
--- Workout Tracker/workoutButtons.swift
+++ Workout Tracker/workoutButtons.swift
@@ -10,63 +10,65 @@ import SwiftUI
 
 struct workoutButtons: View {
     var body: some View {
-        
     NavigationView {
+        ZStack {
+            Color(red: 27 / 255, green: 28 / 255, blue: 30 / 255)
+            .edgesIgnoringSafeArea(.all)
         VStack {
             Text("TIME TO WORKOUT!")
+                .foregroundColor(.white)
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .offset(y:-75)
             Text("Today's Date: November 27 2019")
+                .foregroundColor(.white)
                 .font(.subheadline)
                 .fontWeight(.bold)
                 .offset(y:-50)
         HStack {
             NavigationLink(destination: Calendar()) {
                 Text("PUSH")
-                    .foregroundColor(.blue)
-                    .fontWeight(.bold)
                     .foregroundColor(.white)
+                    .fontWeight(.bold)
                     .font(.title)
                     .padding()
                     .cornerRadius(40)
                     .overlay(
                     RoundedRectangle(cornerRadius: 40)
-                        .stroke(Color.blue, lineWidth: 3.5)
+                        .stroke(Color(red: 155 / 255, green: 130 / 255, blue: 255 / 255), lineWidth: 3.5)
                 )
             }
             Spacer()
             NavigationLink(destination: Text("Hey there!")) {
                 Text("PULL")
-                    .foregroundColor(.blue)
-                    .fontWeight(.bold)
                     .foregroundColor(.white)
+                    .fontWeight(.bold)
                     .font(.title)
                     .padding()
                     .cornerRadius(40)
                     .overlay(
                     RoundedRectangle(cornerRadius: 40)
-                        .stroke(Color.blue, lineWidth: 3.5)
+                        .stroke(Color(red: 155 / 255, green: 130 / 255, blue: 255 / 255), lineWidth: 3.5)
                 )
             }
             Spacer()
             NavigationLink(destination: Text("Hey there!")) {
                 Text("OTHER")
-                    .foregroundColor(.blue)
-                    .fontWeight(.bold)
                     .foregroundColor(.white)
+                    .fontWeight(.bold)
                     .font(.title)
                     .padding()
                     .cornerRadius(40)
                     .overlay(
                     RoundedRectangle(cornerRadius: 40)
-                        .stroke(Color.blue, lineWidth: 3.5)
+                        .stroke(Color(red: 155 / 255, green: 130 / 255, blue: 255 / 255), lineWidth: 3.5)
                 )
             }
             
                 } .padding()
             }
         }
+    }
     }
 }
 
